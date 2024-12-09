5ee37e9730e54e922fb2e946b0e970a669fd4710
gacoelho <gtorres2003@gmail.com>
2024-12-09 05:26:54 -0300

    show case de desligamento feito

--- actions/actions.py
+++ actions/actions.py
@@ -1,8 +1,7 @@
 from rasa_sdk import Action, Tracker
 from typing import Any, Text, Dict, List
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet, FollowupAction
-
+from rasa_sdk.events import SlotSet, FollowupAction, UserUtteranceReverted
 # Ação para colocar o usuário na fila prioritária
 class ActionColocarNaFilaPrioritaria(Action):
     def name(self) -> str:
@@ -14,10 +13,25 @@ class ActionColocarNaFilaPrioritaria(Action):
         # Vamos usar uma variável ou banco de dados para gerenciar isso em um cenário real
         line_number = 1  # Exemplo estático para o número da fila
 
-        dispatcher.utter_message(text=f"Sua chamada foi registrada como urgente e você é {line_number} da fila prioritária.")
+        dispatcher.utter_message(text=f"Sua chamada foi registrada como urgente e você é {line_number}º da fila prioritária.")
 
         # Atualizando o slot 'urgencia' para True, indicando que o usuário foi colocado na fila de urgência
         return [SlotSet("urgencia", True)]
+    
+class ActionColocarNaFila(Action):
+    def name(self) -> str:
+        return "action_colocar_na_fila"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict) -> list:
+        # Simulando a adição do usuário à fila de urgência
+        # Aqui, podemos melhorar a lógica para calcular dinamicamente o número da fila
+        # Vamos usar uma variável ou banco de dados para gerenciar isso em um cenário real
+        line_number = 1  # Exemplo estático para o número da fila
+
+        dispatcher.utter_message(text=f"Sua chamada foi registrada e você é {line_number}º da fila.")
+
+        # Atualizando o slot 'urgencia' para True, indicando que o usuário foi colocado na fila de urgência
+        return []
 
 # Ação para salvar os detalhes do problema
 class ActionSaveProblemDetails(Action):
@@ -33,8 +47,6 @@ class ActionSaveProblemDetails(Action):
             dispatcher.utter_message(text=f"Eu não entedi seu problema")
         # Salvando a descrição no slot 'descricao_problema'
         return [SlotSet("descricao_problema", descricao), FollowupAction('utter_acknowledge')]
-
-
 
 class ActionSaveFeedBack(Action):
     def name(self):
@@ -81,7 +93,61 @@ class ActionDesenvolvimento(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         dispatcher.utter_message(text="Entendido, você quer falar sobre Desenvolvimento")
 
+class ActionAskMotivo(Action):
+    def name(self):
+        return "action_desenvolvimento"
+    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Entendido, você quer falar sobre Desenvolvimento")
 
+class ActionFetchEmployeeAnalysis(Action):
+    def name(self) -> Text:
+        return "action_fetch_employee_analysis"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        nome_funcionario = tracker.get_slot("nome_funcionario")
+
+        # Simulando uma análise do funcionário
+        if nome_funcionario.lower() == "joão":
+            analysis = "Análise de desempenho do funcionário João: desempenho satisfatório, comprometimento com a equipe."
+        elif nome_funcionario.lower() == "maria":
+            analysis = "Análise de desempenho da funcionária Maria: excelente desempenho, atualmente grávida."
+        elif nome_funcionario.lower() == "carlos":
+            analysis = "Análise de desempenho do funcionário Carlos: membro do SIPA (Serviço de Integração e Processamento de Atividades), comprometido com a segurança do trabalho."
+        elif nome_funcionario.lower() == "ana":
+            analysis = "Análise de desempenho da funcionária Ana: desempenho regular, precisa de melhorias na comunicação."
+        else:
+            analysis = f"Análise de desempenho do funcionário {nome_funcionario}: dados indisponíveis."
+
+        dispatcher.utter_message(text=analysis)
+
+        return []
+
+class ActionProcessDismissal(Action):
+    def name(self) -> Text:
+        return "action_process_dismissal"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        nome_funcionario = tracker.get_slot("nome_funcionario")
+        motivo_desligamento = tracker.get_slot("motivo_desligamento")
+
+        # Processamento do desligamento
+        dispatcher.utter_message(text=f"Motivo: {motivo_desligamento}.")
+
+        # Opcional: redefinir os slots
+        return [SlotSet("nome_funcionario", None), SlotSet("motivo_desligamento", None), FollowupAction('action_colocar_na_fila')]
+
+
+class ActionDefaultFallback(Action):
+    def name(self) -> str:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker, domain):
+        dispatcher.utter_message(
+            text="Desculpe, não entendi. Estou aqui para ajudar com assuntos relacionados à carreira, cultura, desligamentos e outros tópicos relacionados."
+        )
+        # Reverte a última interação do usuário para tentar novamente
+        return [UserUtteranceReverted()]
 
 
 
